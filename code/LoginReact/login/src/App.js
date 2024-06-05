d33b2d2af81199bb30e62cf7de67be6ca9416cf7
EvanBinu <evanbinu17@gmail.com>
2024-06-05 21:19:09 +0530

    login react

---
+++ code/LoginReact/login/src/App.js
@@ -0,0 +1,11 @@
+import loginpage from './loginpage/login';
+
+function App(){
+    return(
+        <div className='App'>
+            <loginpage/>
+        </div> 
+    )
+}
+
+export default App;
